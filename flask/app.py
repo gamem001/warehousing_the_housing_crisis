3865ba07c8beaf83a84b9b0dc5796ea5b0c2af60
Asia-Hewett <asia.hewett0046@gmail.com>
2020-11-05 20:32:47 -0600

    Committing Changes to Grab Data

--- flask/app.py
+++ flask/app.py
@@ -1,37 +1,9 @@
 from flask import Flask, jsonify, g
-import json
-import os.path
-import sqlite3
-from werkzeug.utils import secure_filename # This tool allows us to make sure user uploaded files are safe
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__) # This is the name of the module running in flask
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-DATABASE = '../sqlite/all_avg_sfh_price.db'
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-# @app.teardown_appcontext
-# def close_connection(exception):
-#     db = getattr(g, '_database', None)
-#     if db is not None:
-#         db.close()
-
-# @app.route('/') # This is for someone visiting the home page
-# def home():
-#     cur = get_db().cursor().
-
-# def make_dicts(cursor, row):
-#     return dict((cursor.description[idx][0], value)
-#                 for idx, value in enumerate(row))
-
-# db.row_factory = make_dicts
-
-# def query_db(query, args=(), one=False):
-#     cur = get_db().execute(query, args)
-#     rv = cur.fetchall()
-#     cur.close()
-#     return (rv[0] if rv else None) if one else rv
+if __name__ == '__main__':
+    app.run(debug=True)
